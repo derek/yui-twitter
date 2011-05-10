402042b9c8e72b2b36dc8d52ab9e7a276f34a913
Derek Gathright <drgath@gmail.com>
2011-05-10 01:57:46 -0700

    Small tweaks.  added twitter.user.mentions.

--- js/yui-twitter.js
+++ js/yui-twitter.js
@@ -1,25 +1,34 @@
 YUI.add("Twitter", function(Y){
     
-    Y.Twitter = function(){};
+    Y.Twitter = function(){
+        // Nuttin yet
+    };
    
-    Y.Twitter.prototype.search = function(query, cb) {
+// ******* Public
+    Y.Twitter.prototype.search = function(searchQuery, cb) {
         var yqlQuery = 'SELECT * FROM twitter.search WHERE q = @query';
         _execYql(yqlQuery, function(r){
-            cb(r.query.results.results);
-        }, {query: query});
+            cb(r.results.results);
+        }, {query: searchQuery});
     }
 
     Y.Twitter.prototype.user = {};
-    Y.Twitter.prototype.user.timeline = function(user, cb) {
-        var yqlQuery = 'select * from twitter.user.timeline WHERE screen_name = @screen_name';
+    Y.Twitter.prototype.user.timeline = function(userName, cb) {
+        var yqlQuery = 'SELECT * FROM twitter.user.timeline WHERE screen_name = @screen_name';
         _execYql(yqlQuery, function(r){
-            cb(r.query.results.statuses.status)
-        }, {screen_name: user});
+            cb(r.results.statuses.status)
+        }, {screen_name: userName});
+    }
+    
+    Y.Twitter.prototype.user.mentions = function(userName, cb) {
+        /* Temp hack to pull from the public search */
+        new Y.Twitter().search("@derek", cb)
     }
 
+// ******* Private
     function _execYql(query, cb, params) {
         new Y.YQL(query, function(response){
-            cb(response);
+            cb(response.query);
         }, params);
     }
     
