139f228df8d0fa35e6d8e8f435ed0b1df3d0c29f
Derek Gathright <drgath@gmail.com>
2011-05-14 03:43:20 -0500

    Big changes to add authenticated calls and some other goodies.

--- js/yui-twitter.js
+++ js/yui-twitter.js
@@ -1,35 +1,63 @@
 YUI.add("Twitter", function(Y){
     
-    Y.Twitter = function(){
-        // Nuttin yet
+    var Twitter = function(config){
+        this.config = config;
     };
-   
-// ******* Public
-    Y.Twitter.prototype.search = function(searchQuery, cb) {
-        var yqlQuery = 'SELECT * FROM twitter.search WHERE q = @query';
-        _execYql(yqlQuery, function(r){
-            cb(r.results.results);
-        }, {query: searchQuery});
-    }
-
-    Y.Twitter.prototype.user = {};
-    Y.Twitter.prototype.user.timeline = function(userName, cb) {
-        var yqlQuery = 'SELECT * FROM twitter.user.timeline WHERE screen_name = @screen_name';
-        _execYql(yqlQuery, function(r){
-            cb(r.results.statuses.status)
-        }, {screen_name: userName});
+    
+    
+    Twitter.prototype = {
+        
     }
     
-    Y.Twitter.prototype.user.mentions = function(userName, cb) {
-        /* Temp hack to pull from the public search */
-        new Y.Twitter().search("@derek", cb)
+    // ******* Public
+    Twitter.prototype.search = function(searchQuery, cb) {
+        
+        var yqlQuery = 'SELECT * FROM twitter.search WHERE q = @query';
+        this._execYql(yqlQuery, function(r){
+            cb(r.results.results);
+        }, {query: searchQuery});
+
+        return this;
+    }
+
+    Twitter.prototype.friends = function(cb) {
+
+        var yqlQuery = 'SELECT * FROM twitter.status.timeline.friends WHERE oauth_token = @oauth_token AND oauth_token_secret = @oauth_token_secret AND oauth_consumer_key = @oauth_consumer_key AND oauth_consumer_secret = @oauth_consumer_secret';
+        this._execYql(yqlQuery, function(r){
+            cb(r);
+        });
+
+        return this;
+    }
+
+    Twitter.prototype.user_timeline = function(userName, cb) {
+        
+        var yqlQuery = 'SELECT * FROM twitter.user.timeline WHERE screen_name = @screen_name';
+        this._execYql(yqlQuery, function(r){
+            cb(r.results.statuses.status)
+        }, {screen_name: userName});
+
+        return this;
+    }
+
+    Twitter.prototype.user_mentions = function(userName, cb) {
+        
+        // Temp hack to pull from the public search
+        this.search("@"+userName, cb);
+
+        return this;
     }
 
 // ******* Private
-    function _execYql(query, cb, params) {
+    Twitter.prototype._execYql = function(query, cb, params) {
+        params = Y.mix(params || {}, this.config);
         new Y.YQL(query, function(response){
             cb(response.query);
-        }, params);
+        }, params, {proto: "https"});
     }
+
     
+    Y.Twitter = function(config) {
+        return new Twitter(config);
+    };
 });
