0f50339999bb0d826eb8e4700372f54132f4322a
Derek Gathright <drgath@gmail.com>
2011-05-17 01:26:17 -0700

    Simplified queries & param bindings, as well as a few other tweaks.

--- js/yui-twitter.js
+++ js/yui-twitter.js
@@ -1,8 +1,9 @@
 YUI.add("Twitter", function(Y){
     
     var Twitter = function(config){
-        var config = config || false,
-            oauth = false;
+        config = config || false;
+        
+        var oauth = false;
 
         if (config.oauth_token && config.oauth_token_secret && config.oauth_consumer_key && config.oauth_consumer_secret) {
             oauth = {
@@ -16,121 +17,125 @@ YUI.add("Twitter", function(Y){
         this.oauth = oauth;
     };
 
-    Twitter.prototype = {
-        
-    }
 
 // ******* Public
     Twitter.prototype.search = function(searchQuery, callback) {
-        var yqlQuery = 'SELECT * FROM twitter.search WHERE q = @query';
+        
+        // We can't do proper param binding because q (search query) conflicts with q (yql query) in the querystring. :(
+        var yqlQuery = 'twitter.search WHERE q = \'' + searchQuery + '\'';
         this._execYql(yqlQuery, function(r){
             if (callback) callback(r.results.results);
-        }, {query: searchQuery});
-
+        });
+        
         return this;
     }
 
     Twitter.prototype.friends = function(callback) {
-
-        var yqlQuery = 'SELECT * FROM twitter.status.timeline.friends WHERE oauth_token = @oauth_token AND oauth_token_secret = @oauth_token_secret AND oauth_consumer_key = @oauth_consumer_key AND oauth_consumer_secret = @oauth_consumer_secret';
-        this._execYql(yqlQuery, function(r){
-            if (callback) callback(r);
-        }, {}, true);
-
-        return this;
-    }
-
-    Twitter.prototype.user_timeline = function(userName, callback) {
-        
-        var yqlQuery = 'SELECT * FROM twitter.user.timeline WHERE screen_name = @screen_name';
-        this._execYql(yqlQuery, function(r){
+        this._execYql('twitter.status.timeline.friends', function(r){
             if (callback) callback(r.results.statuses.status)
-        }, {screen_name: userName});
-
+        }, {}, true);
+        
         return this;
     }
 
-    Twitter.prototype.user_mentions = function(userName, callback) {
+    Twitter.prototype.user_timeline = function(userName, options, callback) {
         
-        // Temp hack to pull from the public search
-        this.search("@"+userName, callback);
+        options = options || {};
+        
+        var since_id = options.since_id || 1;
 
+        this._execYql('twitter.user.timeline', function(r){
+            if (callback) callback(r.results.statuses.status)
+        }, {screen_name: userName, since_id: since_id});
+        
+        return this;
+    }
+
+    Twitter.prototype.user_mentions = function(callback) {
+        this._execYql('twitter.status.mentions', function(r){
+            if (callback) callback(r.results.statuses.status)
+        }, {}, true);
+        
         return this;
     }
 
     Twitter.prototype.user_favorites = function(callback) {
-        
-        var yqlQuery = 'SELECT * FROM twitter.favorites WHERE oauth_token = @oauth_token AND oauth_token_secret = @oauth_token_secret AND oauth_consumer_key = @oauth_consumer_key AND oauth_consumer_secret = @oauth_consumer_secret';
-        this._execYql(yqlQuery, function(r){
+        this._execYql('twitter.favorites', function(r){
             if (callback) callback(r.results) // TODO: Fix path
         }, {}, true);
-
+        
         return this;
     }
 
     Twitter.prototype.user_lists = function(username, callback) {
-        
-        var yqlQuery = 'SELECT * FROM twitter.lists WHERE user = @user AND oauth_token = @oauth_token AND oauth_token_secret = @oauth_token_secret AND oauth_consumer_key = @oauth_consumer_key AND oauth_consumer_secret = @oauth_consumer_secret';
-        this._execYql(yqlQuery, function(r){
-            if (callback) callback(r.results) // TODO: Fix path
+        this._execYql('twitter.lists', function(r){
+            if (callback) callback(r.results.lists_list.lists.list);
         }, {user:username}, true);
-
+        
         return this;
     }
 
     Twitter.prototype.user_list_subscriptions = function(username, callback) {
-        
-        var yqlQuery = 'SELECT * FROM twitter.lists.subscriptions WHERE user = @user AND oauth_token = @oauth_token AND oauth_token_secret = @oauth_token_secret AND oauth_consumer_key = @oauth_consumer_key AND oauth_consumer_secret = @oauth_consumer_secret';
-        this._execYql(yqlQuery, function(r){
-            if (callback) callback(r.results) // TODO: Fix path
+        this._execYql('twitter.lists.subscriptions', function(r){
+            if (callback) callback(r.results.lists_list.lists.list);
         }, {user:username}, true);
-
+        
         return this;
     }
 
     Twitter.prototype.direct_messages_in = function(callback) {
-        
-        var yqlQuery = 'SELECT * FROM twitter.directmessages WHERE oauth_token = @oauth_token AND oauth_token_secret = @oauth_token_secret AND oauth_consumer_key = @oauth_consumer_key AND oauth_consumer_secret = @oauth_consumer_secret';
-        this._execYql(yqlQuery, function(r){
-            if (callback) callback(r.results) // TODO: Fix path
+        this._execYql('twitter.directmessages', function(r){
+            if (callback) callback(r.results['direct-messages']['direct_message'])
         }, {}, true);
-
+        
         return this;
     }
 
     Twitter.prototype.direct_messages_out = function(callback) {
-        
-        var yqlQuery = 'SELECT * FROM twitter.directmessages.sent WHERE oauth_token = @oauth_token AND oauth_token_secret = @oauth_token_secret AND oauth_consumer_key = @oauth_consumer_key AND oauth_consumer_secret = @oauth_consumer_secret';
-        this._execYql(yqlQuery, function(r){
-            if (callback) callback(r.results) // TODO: Fix path
+        this._execYql('twitter.directmessages.sent', function(r){
+            if (callback) callback(r.results['direct-messages']['direct_message'])
         }, {}, true);
-
+        
         return this;
     }
 
+
 // ******* Private
-    Twitter.prototype._execYql = function(query, callback, params, authenticated) {
-        var query         = query         || false,
-            callback      = callback      || function(){},
-            params        = params        || {},
-            authenticated = authenticated || false;
+    Twitter.prototype._execYql = function(datatable, callback, params, authenticated) {
         
-        if (!query) {
-            throw "Invalid YQL query";
-        }
+        // Defaults
+        datatable     = datatable     || false;
+        callback      = callback      || false;
+        params        = params        || {};
+        authenticated = authenticated || false;
+        
+        // Construct the WHERE portion of the query
+        var where = [];
         
         if (authenticated) {
             params = Y.mix(params, this.oauth);
-        }        
+        }
+        
+        for(key in params) {
+            where.push(key + " = @" + key)
+        }
+        
+        if (where.length) {
+            where = " WHERE " + where.join(" AND ");
+        }
+        
+        query = "SELECT * FROM " + datatable + where;
+        
+        //Y.log(query);
         
         new Y.YQL(query, function(response){
             callback(response.query);
         }, params, {proto: "https"});
-        
     }
-
     
     Y.Twitter = function(config) {
         return new Twitter(config);
     };
+
+
 });
